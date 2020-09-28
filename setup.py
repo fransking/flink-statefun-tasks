c2119e39faf9c86067c0c2ae8ed40bc3012370c4
Frans King <frans.king@sbbsystems.com>
2020-09-28 13:04:05 +0100

    Adding packaging scripts

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = ['apache-flink-statefun']
+
+setuptools.setup(
+    name="statefun-tasks", # Replace with your own username
+    version="0.1.0",
+    author="Frans King",
+    author_email="frans.king@sbbsystems.com",
+    description="Tasks API for Stateful Functions on Flink",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fransking/flink-statefun-tasks",
+    packages=['statefun_tasks'],
+    license='https://www.apache.org/licenses/LICENSE-2.0',
+    license_files=["LICENSE"],
+    install_requires=install_requires,
+    python_requires='>=3.5',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7']
+)
