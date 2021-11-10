7118971640b5eb8855354678b0b771261956d7b1
Frans King <frans.king@sbbsystems.com>
2021-11-10 17:45:56 +0000

    Added is_pipeline to on_task_finished event handler

--- statefun_tasks/events/event_handlers.py
+++ statefun_tasks/events/event_handlers.py
@@ -28,8 +28,8 @@ class EventHandlers(object):
     def notify_task_started(self, context, task_request: TaskRequest):
         self._notify_all(self._on_task_started_handlers, context, task_request)
 
-    def notify_task_finished(self, context, task_result: TaskResult, task_exception: TaskException, pipeline: Pipeline):
-        self._notify_all(self._on_task_finished_handlers, context, task_result=task_result, task_exception=task_exception, pipeline=pipeline)
+    def notify_task_finished(self, context, task_result: TaskResult, task_exception: TaskException, is_pipeline: bool):
+        self._notify_all(self._on_task_finished_handlers, context, task_result=task_result, task_exception=task_exception, is_pipeline=is_pipeline)
 
     def notify_task_retry(self, context, task_request: TaskRequest, retry_count):
         self._notify_all(self._on_task_retry_handlers, context, task_request, retry_count)
