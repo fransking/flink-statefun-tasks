f78ce8cc311b5a06154c9d40914df1061e6c7579
Frans King <frans.king@sbbsystems.com>
2021-11-18 17:53:08 +0000

    Adding nested pipeline inline task test

--- tests/test_inline_tasks.py
+++ tests/test_inline_tasks.py
@@ -19,9 +19,17 @@ class InlineTasksTests(unittest.TestCase):
         def say_goodbye(greeting):
             return f'{greeting} and goodbye'
 
+        @inline_task()
+        def wf(name):
+            return say_hello.send(name).continue_with(say_goodbye)
+
         pipeline = say_hello.send('Jane').continue_with(say_goodbye)
         result = self.test_harness.run_pipeline(pipeline)
         self.assertEqual(result, 'Hello Jane and goodbye')
+
+        pipeline = wf.send('Joe')
+        result = self.test_harness.run_pipeline(pipeline)
+        self.assertEqual(result, 'Hello Joe and goodbye')
 
 if __name__ == '__main__':
     unittest.main()
