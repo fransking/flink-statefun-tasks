50e51e9110f733c04156a95ca64cbc9012a79bd3
Frans King <frans.king@sbbsystems.com>
2021-11-25 12:46:04 +0000

    Improving result aggregation performance

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setuptools.setup(
     name="statefun-tasks",
-    version="0.9.49",
+    version="0.9.50",
     author="Frans King & Luke Ashworth",
     author_email="frans.king@sbbsystems.com",
     description="Tasks API for Stateful Functions on Flink",
