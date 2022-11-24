6f163f458be91c9fb2191f6cfcc951da331e50a4
Frans King <frans.king@sbbsystems.com>
2022-11-24 20:09:20 +0000

    updating version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setuptools.setup(
     name="statefun-tasks",
-    version="0.8.88",
+    version="0.8.89",
     author="Frans King & Luke Ashworth",
     author_email="frans.king@sbbsystems.com",
     description="Tasks API for Stateful Functions on Flink",
