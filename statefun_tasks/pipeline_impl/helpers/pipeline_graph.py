27385a78a8be060936ad163cf97df558d87aee0c
Frans King <frans.king@sbbsystems.com>
2023-03-03 19:14:45 +0000

    Fixing edge case with exceptionally tasks following on from groups

--- statefun_tasks/pipeline_impl/helpers/pipeline_graph.py
+++ statefun_tasks/pipeline_impl/helpers/pipeline_graph.py
@@ -49,7 +49,7 @@ class PipelineGraph(object):
         raise ValueError(f'Task {task_id} not found')
 
     def is_empty(self):
-        tasks, _ ,_  = self.get_initial_tasks()
+        tasks, _ ,_ = self.get_initial_tasks()
         return not any(tasks)
         
     def get_task_chain(self, task_id):
@@ -164,7 +164,7 @@ class PipelineGraph(object):
                     task.mark_complete()
 
         return True
-   
+         
     def try_get_finally_task(self, task_id=None):
         finally_task = next((task for task in self._pipeline if isinstance(task, Task) and task.is_finally), None)
         return None if finally_task is None or finally_task.uid == task_id else finally_task
@@ -175,17 +175,14 @@ class PipelineGraph(object):
 
     def get_next_step_in_pipeline(self, task_id, task_result_or_exception):
         current_step, next_step, group, empty_group = self._get_next_step_in_pipeline(task_id)
-        
-        skipped_tasks = []
+
 
         if isinstance(task_result_or_exception, TaskException):
             # if we have a TaskException as input then we need to skip forward to the next exceptionally
             # task if we have one or otherwise to the finally task if we have one
-            task = next_step
-
-            while task is not None and isinstance(task, Task) and not task.is_exceptionally:
-                skipped_tasks.append(task)
-                _, task, _, _ = self._get_next_step_in_pipeline(task.uid)
+            
+            # this is made more complicated by having possibly nested groups which also need to be skipped over
+            task, skipped_tasks = self._skip_over_tasks_and_groups(next_step)
 
             next_step = task or next_step
 
@@ -201,14 +198,37 @@ class PipelineGraph(object):
                     next_step = self.try_get_finally_task(task_id)
 
         else:
-            # otherwise we have a TaskResult and so we need to skip over exceptionally tasks
-            while next_step is not None and isinstance(next_step, Task) and next_step.is_exceptionally:
-                next_step.mark_complete()
-                skipped_tasks.append(next_step)
-
-                _, next_step, _, _ = self._get_next_step_in_pipeline(next_step.uid)
+            # otherwise we have a TaskResult and so we need to skip over exceptionally tasksv
+            next_step, skipped_tasks = self._skip_over_exceptionally_tasks(next_step)
 
         return current_step, next_step, group, empty_group, skipped_tasks
+
+    def _skip_over_tasks_and_groups(self, task):
+        skipped_tasks = []
+
+        while task is not None:
+
+            if isinstance(task, Group):
+                skipped_tasks.extend(self.yield_tasks([task]))
+                _, task, _, _ = self._get_next_step_in_pipeline(skipped_tasks[-1].uid)
+
+            elif isinstance(task, Task) and not task.is_exceptionally:
+                skipped_tasks.append(task)
+                _, task, _, _ = self._get_next_step_in_pipeline(task.uid)
+            else:
+                break
+
+        return task, skipped_tasks
+
+    def _skip_over_exceptionally_tasks(self, task):
+        skipped_tasks = []
+
+        while task is not None and isinstance(task, Task) and task.is_exceptionally:
+            task.mark_complete()
+            skipped_tasks.append(task)
+            _, task, _, _ = self._get_next_step_in_pipeline(task.uid)
+        
+        return task, skipped_tasks
 
     def _get_next_step_in_pipeline(self, task_id):
         stack = deque([(self._pipeline, None, None, None, None)])  # FIFO (pipeline, entry after group, group, parent_group, entry after parent group)
